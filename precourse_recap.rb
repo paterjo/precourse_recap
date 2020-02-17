e9ee8f90d70a1b55614607f1cf28d524daa1cd23
moira <johnandmoira@live.co.u>
2020-02-17 16:24:59 +0000

    added ruby examples

---
+++ precourse_recap.rb
@@ -0,0 +1,26 @@
+weather = "sunny"
+if weather == "rainy"
+  print "Need to take my umbrella"
+else
+  print "Need to wear my sunglasses"
+end
+
+def greet
+  p "Hello"
+end
+
+musical_instruments = ["guitar", "drum","piano"]
+
+dog = labrador
+weight = 30
+colour = black
+
+pet = { "dog" => "labrador", "weight" => 30, "colour" => "black"}
+
+counter = 1
+target = 7
+
+while counter <= target
+  p "counter is #{counter}"
+  counter = counter + 1
+end
